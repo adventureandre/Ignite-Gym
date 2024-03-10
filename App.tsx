df9d676aeaa4034dac044df8628b48c9deeef99d
Andre Luiz <adventureandre@hotmail.com>
2024-03-10 00:23:39 -0300

    token de cores Fixes #7

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
 
 import {StatusBar } from 'react-native';
-import {NativeBaseProvider}from 'native-base'
+import {NativeBaseProvider, View}from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
+import { THEME } from 'src/theme';
 
 export default function App() {
 
  const [fontsLoaded] = useFonts({Roboto_400Regular,Roboto_700Bold})
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
       barStyle="light-content"
       backgroundColor="transparent"
       translucent
       />
-       <Loading/>
+       {fontsLoaded ? <View/> :<Loading/>}
     </NativeBaseProvider>
   );
 }
