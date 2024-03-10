6ff593ef77ea9ee12029b0ab7ab7babb730f0f02
Andre Luiz <adventureandre@hotmail.com>
2024-03-10 00:11:59 -0300

    utilizando nativebase

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 
-import { View,StatusBar } from 'react-native';
+import {StatusBar } from 'react-native';
 import {NativeBaseProvider}from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 
 export default function App() {
 
@@ -14,8 +15,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
-      {fontsLoaded ? 
-      <View/> : <View/>}
+       <Loading/>
     </NativeBaseProvider>
   );
 }
---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import { Center, Spinner } from "native-base";
+
+export function Loading() {
+    return (
+        <Center flex={1}>
+            <Spinner />
+        </Center>
+
+    )
+}
