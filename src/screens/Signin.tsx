a4ed4fd48239543841292593e7a4ae1b35585f16
Andre Luiz <adventureandre@hotmail.com>
2024-03-11 22:57:47 -0300

    Componente Heading Fixes #13

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,4 +1,4 @@
-import { VStack, Image, Text, Center } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
 import LogoSvg from "@assets/logo.svg"
 import BackgroundImg from '@assets/background.png';
@@ -16,6 +16,12 @@ export function SignIn() {
                     Treine sua mente e o seu corpo
                 </Text>
             </Center>
+            <Center>
+                <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+                    Acesse sua conta
+                </Heading>
+            </Center>
+
         </VStack>
     );
 }
