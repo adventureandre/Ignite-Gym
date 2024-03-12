657b18d8f3c90fcc2224dd4b4ffac3e9b6166194
Andre Luiz <adventureandre@hotmail.com>
2024-03-11 22:26:16 -0300

    Imagem de Background Fixes #9

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 
 import {StatusBar } from 'react-native';
-import {NativeBaseProvider, View}from 'native-base'
+import {NativeBaseProvider}from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { THEME } from 'src/theme';
+import { SignIn } from '@screens/Signin';
 
 export default function App() {
 
@@ -16,7 +17,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
-       {fontsLoaded ? <View/> :<Loading/>}
+       {fontsLoaded ? <SignIn/> :<Loading/>}
     </NativeBaseProvider>
   );
 }
