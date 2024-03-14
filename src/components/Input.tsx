2c8dac187d5b022ea15049cedf4015c0cd13e047
Andre Luiz <adventureandre@hotmail.com>
2024-03-13 21:40:04 -0300

    Finalizar os inputs do signin Fixes #18

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -14,6 +14,11 @@ export function Input({...rest}:Props) {
             fontFamily="body"
             mb={4}
             placeholderTextColor="gray.300"
+            _focus={{
+                bg:"gray.700",
+                borderWidth:1,
+                borderColor:"green.500"
+            }}
             {...rest}
 
         />
